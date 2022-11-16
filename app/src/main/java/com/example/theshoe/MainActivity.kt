fa8e68afee7d876ea1a43fd1d90512c82e59e41e
Mustafa Raafat <mustafa.raft@hotmail.com>
2022-11-16 10:50:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/theshoe/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.theshoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
