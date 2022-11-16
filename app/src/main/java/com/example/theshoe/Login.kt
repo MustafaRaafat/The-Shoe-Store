73be1b838b6ee4afa9d06c40762664b112eac586
Mustafa Raafat <mustafa.raft@hotmail.com>
2022-11-16 14:18:21 +0200

    create main navigation xml file with five destinations.

---
+++ app/src/main/java/com/example/theshoe/Login.kt
@@ -0,0 +1,23 @@
+package com.example.theshoe
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.theshoe.databinding.FragmentLoginBinding
+
+class Login : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentLoginBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/theshoe/ShoeList.kt
@@ -0,0 +1,23 @@
+package com.example.theshoe
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.theshoe.databinding.FragmentShoeListBinding
+
+class ShoeList : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentShoeListBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_list, container, false)
+        return binding.root
+    }
+
+}
